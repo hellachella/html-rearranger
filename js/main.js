22ab3c7753e598ef604cf93053ddca68c6c2309a
Michelle <vand0432@algonquinlive.com>
2016-03-09 15:56:40 -0500

    Starting fresh.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-var $body = $('body');
-var $h2a = $('<h2>');
-var $h2b = $('<h2>')
 
 
-$h2a.html('Below Ground Veggies');
-
-$body.prepend($h2a);
-
-$h2b.html('Above Ground Veggies')
-$body.append($h2b);
 
