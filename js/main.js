d40794824f81fabf1aa5e3b2f5a3898d19b929cd
Michelle <vand0432@algonquinlive.com>
2016-03-09 16:33:13 -0500

    Whoa.  Something happened, something that made it work?

--- js/main.js
+++ js/main.js
@@ -11,9 +11,17 @@
 var $body = $('body');
 var $headerB = $('<h2>');
 var $ulB = $('<ul>');
+var $ul = $('ul');
+var $li = $('ul li');
 
 $body.append($headerB);
 $headerB.html('Below Ground Veggies');
 $body.append($ulB);
 
-
+$li.each(function () {
+  if ($(this).hasClass('below')){
+    $ulB.append($(this));
+  }else{
+    alert('nah');
+  }
+});
