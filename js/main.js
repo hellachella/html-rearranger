654f42590c287141e2ee766cab756074519f8c8e
Michelle <vand0432@algonquinlive.com>
2016-03-09 16:38:36 -0500

    Holy Rabies, it worked.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
-//make vars
-
-//loop ul
-
-//if statement.
-
-//go
-
-
-
 var $body = $('body');
 var $headerB = $('<h2>');
 var $ulB = $('<ul>');
+var $headerA = $('<h2>');
+var $ulA = $('<ul>');
 var $ul = $('ul');
 var $li = $('ul li');
 
 $body.append($headerB);
 $headerB.html('Below Ground Veggies');
 $body.append($ulB);
+$body.append($headerA);
+$headerA.html('Above Ground Veggies');
+$body.append($ulA);
 
 $li.each(function () {
   if ($(this).hasClass('below')){
     $ulB.append($(this));
   }else{
-    alert('nah');
+    $ulA.append($(this));
   }
 });
